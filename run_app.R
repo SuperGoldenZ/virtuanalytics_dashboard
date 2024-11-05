2127a9d3fe77825533fd4de530a8663ed757236f
Alexander Golden <4214415+SuperGoldenZ@users.noreply.github.com>
2024-11-05 20:58:33 +0900

    change to use runApp which includes www folder

--- run_app.R
+++ run_app.R
@@ -1,10 +1,6 @@
 library(shiny)
 
-source("R/analytics.R")
-source("ui.R")
-source("server.R")
-
 options(shiny.port = 8080)
 
 # Run the application
-shinyApp(ui = ui, server = server)
+runApp("./")
