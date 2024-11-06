7c6d4313cc0f71750b237fccef072b3f457d9b56
Alexander Golden <4214415+SuperGoldenZ@users.noreply.github.com>
2024-11-07 08:21:46 +0900

    adding p value

--- R/analytics_rounds.R
+++ R/analytics_rounds.R
@@ -49,6 +49,8 @@ how_rounds_won_data <- function(data) {
             EX = round((EX / Total), 5)
         )
 
+
+
     return(rounds)
 }
 
@@ -63,7 +65,9 @@ how_rounds_lost_data <- function(data) {
         mutate(Winner_Character = ifelse(Winning.Player.Number == 1, Player.1.Character, Player.2.Character)) %>%
         mutate(Loser_Character = ifelse(Winning.Player.Number == 2, Player.1.Character, Player.2.Character)) %>%
         select(Match.ID, Player.1.Character, Player.2.Character, Winner_Character, Loser_Character, `How.Round.Ended`) %>%
-        group_by(Loser_Character, How.Round.Ended) %>%
+        group_by(Loser_Character, How.Round.Ended)
+
+    rounds_summary <- rounds %>%
         summarise(count = n(), .groups = "drop") %>%
         pivot_wider(names_from = How.Round.Ended, values_from = count, values_fill = 0) %>%
         rowwise() %>%
@@ -74,7 +78,44 @@ how_rounds_lost_data <- function(data) {
             EX = round((EX / Total), 5)
         )
 
-    return(rounds)
+    rounds <- rounds %>%
+        mutate(is_ko = ifelse(How.Round.Ended == "KO", 1, 0)) %>%
+        mutate(is_ro = ifelse(How.Round.Ended == "RO", 1, 0)) %>%
+        mutate(is_ex = ifelse(How.Round.Ended == "EX", 1, 0))
+
+    rounds_summary$ko_p_value <- NA
+    rounds_summary$ro_p_value <- NA
+    rounds_summary$ex_p_value <- NA
+
+    for (loser_character_name in unique(rounds$Loser_Character)) {
+        ro_specific <- rounds %>% filter(Loser_Character == loser_character_name)
+        ro_specific <- ro_specific$is_ro
+
+        ro_other <- rounds %>% filter(Loser_Character != loser_character_name)
+        ro_other <- ro_other$is_ro
+
+        t_test_result <- t.test(ro_specific, ro_other)
+        rounds_summary$ro_p_value[rounds_summary$Loser_Character == loser_character_name] <- t_test_result$p.value
+
+        ex_specific <- rounds %>% filter(Loser_Character == loser_character_name)
+        ex_specific <- ex_specific$is_ex
+
+        ex_other <- rounds %>% filter(Loser_Character != loser_character_name)
+        ex_other <- ex_other$is_ex
+
+        t_test_result <- t.test(ex_specific, ex_other)
+        rounds_summary$ex_p_value[rounds_summary$Loser_Character == loser_character_name] <- t_test_result$p.value
+
+        ko_specific <- rounds %>% filter(Loser_Character == loser_character_name)
+        ko_specific <- ko_specific$is_ko
+
+        ko_other <- rounds %>% filter(Loser_Character != loser_character_name)
+        ko_other <- ko_other$is_ko
+
+        t_test_result <- t.test(ko_specific, ko_other)
+        rounds_summary$ko_p_value[rounds_summary$Loser_Character == loser_character_name] <- t_test_result$p.value
+    }
+    return(rounds_summary)
 }
 
 how_rounds_end_per_stage <- function(data) {
